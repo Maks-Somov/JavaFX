8b4898a28d9be085f2a382d5536593640cd8d604
Maks-Somov <fail.epic2015@yandex.ru>
2020-04-13 21:38:34 +0300

    add event on click to Регистрация

--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -1,11 +1,17 @@
 package sample.controllers;
 import static java.lang.System.out;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class Controller {
@@ -30,9 +36,23 @@ public class Controller {
 
     @FXML
     void initialize() {
-             authSignButton.setOnAction(event -> {
-           out.println("была нажата кнопка вход");
-       });
+        loginSignUpButton.setOnAction(event -> {
+           loginSignUpButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/SignUp.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
 
     }
 }
